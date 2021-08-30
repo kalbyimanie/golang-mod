44b57d26f497c36583adde95f2d1fc19182cbedf
kalbyimanie <aby.imanie@gmail>
2021-08-31 01:12:35 +0700

    go api testing db connecion

--- api/api.go
+++ api/api.go
@@ -2,30 +2,91 @@ package main
 
 import (
 	"fmt"
+	"golang-mod/api/config"
 	"log"
+
+	"github.com/spf13/viper"
 
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
-func main() {
+/*
+» Reading variables using the model
+fmt.Println("Reading variables using the model..")
+fmt.Println("Database is\t", configuration.Database.DBName)
+fmt.Println("Port is\t\t", configuration.Server.Port)
+fmt.Println("EXAMPLE_PATH is\t", configuration.EXAMPLE_PATH)
+fmt.Println("EXAMPLE_VAR is\t", configuration.EXAMPLE_VAR)
 
-	dsn := "dev1:dev_test@tcp(127.0.0.1:3306)/local?charset=utf8mb4&parseTime=True&loc=Local"
+» Reading variables without using the model
+ fmt.Println("\nReading variables without using the model..")
+ fmt.Println("Database is\t", viper.GetString("database.dbname"))
+ fmt.Println("Port is\t\t", viper.GetInt("server.port"))
+ fmt.Println("EXAMPLE_PATH is\t", viper.GetString("EXAMPLE_PATH"))
+ fmt.Println("EXAMPLE_VAR is\t", viper.GetString("EXAMPLE_VAR"))
+*/
+
+func dbConn(username string, userpass string, dbname string, dbhost string, dbport string) {
+	// dsn := "dev1:dev_test@tcp(127.0.0.1:3306)/local?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := username + ":" + userpass + "@tcp(" + dbhost + ":" + dbport + ")/" + dbname + "?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Printf("%v", err)
-	} else {
-		fmt.Printf("successfully connected")
 	}
-
 	sqlDB, err := db.DB()
 
-	for {
-		if err = sqlDB.Ping(); err != nil {
-			fmt.Printf("error cuk !")
+	if err = sqlDB.Ping(); err != nil {
+		log.Fatal("connection to database error !!!")
+	} else {
+		fmt.Println("connected to database")
+	}
+}
+
+func main() {
+	// Set the file name of the configurations file
+	viper.SetConfigName("config")
+
+	// Set the path to look for the configurations file
+	viper.AddConfigPath(".")
+
+	// Enable VIPER to read Environment Variables
+	viper.AutomaticEnv()
+
+	viper.SetConfigType("yaml")
+	var configuration config.Configurations
+
+	if err := viper.ReadInConfig(); err != nil {
+		fmt.Printf("Error reading config file, %s", err)
+	}
+
+	// Set undefined variables
+	viper.SetDefault("database.dbname", "local")
+
+	err := viper.Unmarshal(&configuration)
+	if err != nil {
+		fmt.Printf("Unable to decode into struct, %v", err)
+	}
+
+	config := map[string]string{
+		"username": viper.GetString("database.dbuser"),
+		"userpass": viper.GetString("database.dbpassword"),
+		"dbname":   viper.GetString("database.dbname"),
+		"dbport":   viper.GetString("database.dbport"),
+		"dbhost":   viper.GetString("database.dbhost"),
+	}
+
+	// validation for db connection
+	for key, val := range config {
+		if len(val) == 0 {
+			log.Fatalf("%s required value\n", key)
 		}
 	}
 
-	// db.DB().ping
+	dbConn(config["username"],
+		config["userpass"],
+		config["dbname"],
+		config["dbhost"],
+		config["dbport"])
 
 }
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.16
 
 require (
 	github.com/gorilla/mux v1.8.0
-	gorm.io/driver/mysql v1.1.1
+	github.com/spf13/viper v1.8.1
+	gorm.io/driver/mysql v1.1.2
 	gorm.io/gorm v1.21.12
 )
